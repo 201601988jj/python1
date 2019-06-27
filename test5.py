520e4cd47c7c43f4c20394021a391bb35dd0d87a
201601988jj <cjyhk96@gmail.com>
2019-06-27 16:27:53 +0900

    4시까지 코딩 연습

---
+++ test5.py
@@ -0,0 +1,14 @@
+
+def myFunc():
+    print('함수를 호출함')
+
+
+gVar=100
+
+
+if __name__ == '__main__':
+    print('메인 함수 부분이 실행됩니다')
+    myFunc()
+    print('전역변수값:', gVar)
+
+
---
+++ test7.py
@@ -0,0 +1,13 @@
+import test5
+import sys
+import random
+import profile
+
+
+
+
+test5.myFunc()
+
+
+
+
